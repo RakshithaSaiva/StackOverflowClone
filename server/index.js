7b9236758c8f899dbabbb0e122864521c28ecd1e
RakshithaSaiva <rakshithasaiva@gmail.com>
2023-09-01 19:27:17 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 
 const app=express();
-dotenv.config();
+ dotenv.config();
  app.use(express.json({limit :"30mb", extended: true}))
  app.use(express.urlencoded({limit: "30mb" , extended: true}))
  app.use(cors());
@@ -23,8 +23,8 @@ dotenv.config();
 
  const PORT= process.env.PORT || 5000
 
- const CONNECTION_URL="mongodb+srv://rakshitha:atlas123@cluster0.x98lcc3.mongodb.net/?retryWrites=true&w=majority"
+ const DATABASE_URL= process.env.CONNECTION_URL
 
- mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology: true })
+ mongoose.connect(DATABASE_URL, {useNewUrlParser:true, useUnifiedTopology: true })
    .then(()=> app.listen(PORT ,()=> { console.log(`server running on port ${PORT}`)}))
    .catch((err)=> console.log(err.message))
