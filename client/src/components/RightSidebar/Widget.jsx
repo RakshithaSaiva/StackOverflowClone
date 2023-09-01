7363845fa83d8f019cd587ebf730c0d5345e12e3
RakshithaSaiva <rakshithasaiva@gmail.com>
2023-09-01 18:40:29 +0530

    third commit

--- client/src/components/RightSidebar/Widget.jsx
+++ client/src/components/RightSidebar/Widget.jsx
@@ -10,34 +10,48 @@ const Widget = () => {
       <h4>The Overflow Blog</h4>
       <div className='right-sidebar-div-1'>
         <div className='right-sidebar-div-2'>
-        <img src={pen} alt='pen' width='18'/>
-        <p>Building a safer community: Announcing our new Code of Conduct</p>
+        <img src={pen} alt='pen' width='12'/>
+        <p>Observability is key to the future of software (and your DevOps career)</p>
          </div>
          <div className='right-sidebar-div-2'>
-        <img src={pen} alt='pen' width='18'/>
-        <p>Balancing a PhD program with a startup career (Ep. 576)</p>
+        <img src={pen} alt='pen' width='12'/>
+        <p>Podcast 374: How valuable is your screen name?</p>
          </div>
       </div>
       <h4>Featured on Meta</h4>
         <div className='right-sidebar-div-1'>
              <div className='right-sidebar-div-2'>
-                  <img src={comment} alt='comment' width='18'/>
-                         <p>AI/ML Tool examples part 3 - Title-Drafting Assistant</p>
+                  <img src={comment} alt='comment' width='12'/>
+                         <p>Review queue workflows - Final release....</p>
              </div>
              <div className='right-sidebar-div-2'>
-                  <img src={comment} alt='comment' width='18'/>
-                         <p>We are graduating the updated button styling for vote arrows</p>
+                  <img src={comment} alt='comment' width='12'/>
+                         <p>Please welcome Valued Associates: #958 - V2Blast #959 - SpencerG</p>
              </div>
             <div className='right-sidebar-div-2'>
-                     <img src={blacklogo} alt='blacklogo' width='18'/>
-                            <p>Does the policy change for AI-generated content affect users who (want to)...</p>
+                     <img src={blacklogo} alt='blacklogo' width='14'/>
+                            <p>Outdated Answers: accepted answer is now unpinned on Stack Overflow</p>
             </div>
          </div>
          <h4>Hot Meta Posts</h4>
         <div className='right-sidebar-div-1'>
              <div className='right-sidebar-div-2'>
-                         <p>16</p>
-                         <p>Discussion: New AI Generated content policy</p>
+                  <p>38</p>
+                  <p>
+                  Why was this spam flag declined, yet the question marked as spam?
+                  </p>
+             </div>
+             <div className='right-sidebar-div-2'>
+                  <p>20</p>
+                  <p>
+                  What is the best course of action when a user has high enough rep to...
+                  </p>
+             </div>
+             <div className='right-sidebar-div-2'>
+                  <p>14</p>
+                  <p>
+                  Is a link to the "How to ask" help page a useful comment?
+                  </p>
              </div>
          </div>
      </div> 
