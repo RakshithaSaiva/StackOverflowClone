18f4271e085f39b232f0764591136bee7cb7cc61
RakshithaSaiva <rakshithasaiva@gmail.com>
2023-09-01 18:12:58 +0530

    second commit

--- client/src/Pages/UserProfile/UserProfile.jsx
+++ client/src/Pages/UserProfile/UserProfile.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBirthdayCake } from "@fortawesome/free-solid-svg-icons";
+import { faBirthdayCake ,faPen} from "@fortawesome/free-solid-svg-icons";
 import moment from "moment";
 import LeftSidebar from "../../components/LeftSidebar/LeftSidebar";
 import Avatar from "../../components/Avatar/Avatar";
+import EditProfileForm from "./EditProfileForm";
+import ProfileBio from "./ProfileBio";
+import './UserProfile.css'
 
 const UserProfile = () => {
   const { id } = useParams();
   const users = useSelector((state) => state.usersReducer);
   const currentProfile = users.filter((user) => user._id === id)[0];
   const currentUser = useSelector((state) => state.currentUserReducer);
+  console.log(currentProfile)
+
+   const [Switch, setSwitch] =useState(false);
 
   console.log(id);
   console.log(users);
@@ -30,19 +36,35 @@ const UserProfile = () => {
                   color="white"
                   fontSize="50px"
                   px="40px"
-                  py="30py"
+                  py="30px"
                 >
                   {currentProfile.name.charAt(0).toUpperCase()}
                 </Avatar>
                 <div className="user-name">
                   <h1>{currentProfile.name}</h1>
                   <p>
-                    <FontAwesomeIcon icon={faBirthdayCake} />
-                    Joined {moment(currentProfile.joinedOn).fromNow()}
+                    <FontAwesomeIcon icon={faBirthdayCake}/> Joined {moment(currentProfile.joinedOn).fromNow()}
                   </p>
                 </div>
               </div>
+              {
+                currentUser.result._id=== id && (
+                  <button type='button' onClick={()=> setSwitch(true)} className="edit-profile-btn"> 
+                      <FontAwesomeIcon icon={faPen} /> Edit Profile
+                  </button>
+                )
+              }
             </div>
+            <>
+              {
+                Switch ? (
+                  <EditProfileForm currentUser={currentUser} setSwitch={setSwitch}/>
+                 ) : (
+                    <ProfileBio currentProfile={currentProfile}/>
+                  )
+                
+              }
+            </>
           </section>
         </div>
       </div>
