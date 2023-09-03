2ca2dfb37f4d491ef9ddf07d416ce8cbafe1014e
RakshithaSaiva <rakshithasaiva@gmail.com>
2023-09-03 19:40:28 +0530

    first commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API= axios.create({baseURL: 'https://stackoverflowclone-production.onrender.com/'})
+const API= axios.create({baseURL: 'https://stack-overflow-9o6z.onrender.com'})
 
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('Profile')){
